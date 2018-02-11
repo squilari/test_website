635f28199622d94b46112b16c470ce6e65e2b2a6
Larissa Squires <larissasquires@Larissas-MacBook-Air.local>
2018-02-11 10:59:48 -0800

    setup for Heroku

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template
+import os
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return render_template('hello.html')
 
-app.run(debug = True)
+port = int(os.environ.get("PORT", 5000))
+app.run(debug = False, host="0.0.0.0", port=port)
