2f96c237e1f657b8805db08f1962ed02ce692d9a
Larissa Squires <larissasquires@Larissas-MacBook-Air.local>
2018-02-18 19:19:51 -0800

    Goodbye button

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return render_template('hello.html')
 
+@app.route('/goodbye')
+def goodbye():
+    return render_template('goodbye.html')
+
 port = int(os.environ.get("PORT", 5000))
 app.run(debug = False, host="0.0.0.0", port=port)
